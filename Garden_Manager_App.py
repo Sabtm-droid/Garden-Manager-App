3f491cccbf7ef19ceb350e178a3526c842c20252
Sabtm-droid <67222764+Sabtm-droid@users.noreply.github.com>
2025-09-02 18:24:19 +0300

    Create Garden_Manager_App.py

---
+++ Garden_Manager_App.py
@@ -0,0 +1,10 @@
+# Imports
+import math
+#This is a sample of the main python file
+# Team Member 1 Code, Abulla: Function #1
+
+# Team Member 2 Code, Salman, Function #2
+
+# Team Member 3 Code, Abdulrahman, Function #3&4
+
+#Team Member 4 Code, Mohammed, Function #5
